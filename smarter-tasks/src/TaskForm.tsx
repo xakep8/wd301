ee6ab349c6ddacaf0aa72eada588e43081efa089
Kunal Dubey <xakep8@protonmail.com>
2023-08-30 19:14:20 +0530

    made required changes

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -46,12 +46,12 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
     return (
         <form onSubmit={this.addTask} className="flex flex-col">
             <label>Task Title</label>
-            <input className="m-2" id='title' type='text' value={this.state.title} onChange={this.titleChanged}/>
+            <input className="m-2" id='todoTitle' type='text' value={this.state.title} onChange={this.titleChanged}/>
             <label>Task Description</label>
             <input className="m-2" id='todoDescription' type='text' value={this.state.todoDescription} onChange={this.descriptionChanged}/>
             <label>Task Duedate</label>
             <input className="m-2" id='todoDueDate' type='text' value={this.state.todoDueDate} onChange={this.dueDateChanged}/>
-            <button type='submit'>Add item</button>
+            <button id='addTaskButton' type='submit'>Add item</button>
         </form>
     )
   }
