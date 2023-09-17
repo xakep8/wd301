f41d93f8a8770502e4ada45fa377962ce42d78ca
Kunal Dubey <xakep8@protonmail.com>
2023-09-17 14:43:13 +0530

    changed the route for the site to redirect to signin

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -11,7 +11,7 @@ import Logout from './pages/logout';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/signup" replace />,
+    element: <Navigate to="/signin" replace />,
   },
   {
     path:"/logout",
