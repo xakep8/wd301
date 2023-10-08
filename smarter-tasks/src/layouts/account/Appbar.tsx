3f8fe0ae112e39268115969d307dc1004068a8a0
Kunal Dubey <xakep8@protonmail.com>
2023-10-08 11:43:29 +0530

    fixed basic build problems

--- smarter-tasks/src/layouts/account/Appbar.tsx
+++ smarter-tasks/src/layouts/account/Appbar.tsx
@@ -5,7 +5,6 @@ import { Link, useLocation } from "react-router-dom"
 import { Disclosure, Menu, Transition, Switch } from '@headlessui/react';
 import { useState,useContext } from 'react';
 import { ThemeContext } from '../../context/theme';
-import './Appbar.css';
 
 const userNavigation = [
   { name: 'Profile', href: '#' },
@@ -74,17 +73,17 @@ const Appbar = () => {
                 </div>
               </div>
               <div className="hidden md:block">
-                <div className="ml-4 flex items-center md:ml-6">
+                <div className="ml-4 inline-flex items-center md:ml-6">
                 <Switch
                     checked={enabled}
                     onChange={toggleTheme}
                     className={`${enabled ? 'bg-slate-400' : 'bg-slate-700'}
-                      relative inline-flex h-[24px] w-[60px] shrink-0 cursor-pointer border-2 rounded-full border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                      relative py-0.5 px-0.5 inline-flex h-[23px] w-[60px] shrink-0 cursor-pointer border-2 rounded-full border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
                   >
                     <span
                       aria-hidden="true"
-                      className={`${enabled ? 'translate-x-4' : 'translate-x-0'}
-                        relative pointer-events-none toggle-ball inline-block h-[16px] w-[16px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                      className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
+                        pointer-events-none inline-block h-[16px] w-[16px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
                     />
                   </Switch>
                   <Menu as="div" className="relative ml-3">
