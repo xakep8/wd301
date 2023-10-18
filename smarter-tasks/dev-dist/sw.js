4501d5cde41f52d50e1a3e669d9a6ae3e4c8608b
Kunal Dubey <xakep8@protonmail.com>
2023-10-18 19:45:14 +0530

    pwa fix

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8t0ft75vrao"
+    "revision": "0.mu0tqobfvgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
