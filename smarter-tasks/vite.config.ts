32a4278c704eac1e0e4195b2bd73e61f03491362
Kunal Dubey <xakep8@protonmail.com>
2023-10-18 21:58:43 +0530

    pwa

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -4,9 +4,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    outDir:"dev-dist",
+  },
   plugins: [
     react(),
     VitePWA({
+      registerType:"autoUpdate",
       devOptions: {
         enabled: true // For making sure that the PWA is testable from the Local dev environment
       },
